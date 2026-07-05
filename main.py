72a39012f5c143da21ac789b095655f406db3d88
Your Name <your-email@example.com>
2026-07-05 21:47:04 +0530

    Added verify endpoint

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import jwt
+from jwt import ExpiredSignatureError
+from jwt import InvalidTokenError
+from fastapi import HTTPException
+
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel
 from starlette.middleware.base import BaseHTTPMiddleware
 import uuid
 import time
@@ -10,6 +17,25 @@ app = FastAPI()
 ALLOWED_ORIGIN = "https://dash-0gz2k3.example.com"
 
 EMAIL = "22f3001083@ds.study.iitm.ac.in"
+
+PUBLIC_KEY = """
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
+EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
+WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
+ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
+SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
+-----END PUBLIC KEY-----
+"""
+
+ISSUER = "https://idp.exam.local"
+
+AUDIENCE = "tds-66eeudqb.apps.exam.local"
+
+class TokenRequest(BaseModel):
+    token: str
 
 app.add_middleware(
     CORSMiddleware,
@@ -48,3 +74,35 @@ async def stats(values: str):
         "max": max(nums),
         "mean": sum(nums)/len(nums)
     }
+
+@app.post("/verify")
+async def verify(request: TokenRequest):
+
+    try:
+
+        payload = jwt.decode(
+            request.token,
+            PUBLIC_KEY,
+            algorithms=["RS256"],
+            issuer=ISSUER,
+            audience=AUDIENCE,
+        )
+
+        return {
+            "valid": True,
+            "email": payload.get("email"),
+            "sub": payload.get("sub"),
+            "aud": payload.get("aud")
+        }
+
+    except (ExpiredSignatureError, InvalidTokenError):
+
+        return JSONResponse(
+            status_code=401,
+            content={
+                "valid": False
+            }
+        )
+
+
+
